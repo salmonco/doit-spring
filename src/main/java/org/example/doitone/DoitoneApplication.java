82c37807c34fbf8791c503276607d12242988dfc
salmonco <xkqjsslsek80@naver.com>
2024-04-02 19:44:52 +0900

    서블릿 수동 등록

--- src/main/java/org/example/doitone/DoitoneApplication.java
+++ src/main/java/org/example/doitone/DoitoneApplication.java
@@ -2,9 +2,10 @@ package org.example.doitone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
-// ComponentScan이 있어서 스프링에 필요한 객체들을 컨테이너에 등록할 수 있다
-@SpringBootApplication
+@ServletComponentScan // 서블릿 수동 등록
+@SpringBootApplication // ComponentScan이 있어서 스프링에 필요한 객체들을 컨테이너에 등록할 수 있다
 public class DoitoneApplication {
 
     public static void main(String[] args) {
